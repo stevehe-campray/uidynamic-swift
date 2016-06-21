4f7c2f6ff352959fb3c1be555812bb80203aea97
stevehe-campray <hejingjin@hejingjin.local>
2016-06-21 10:36:22 +0800

    初始化重力行为学习 swift版本
    初始化重力行为学习 swift版本

---
+++ uidynamic学习/uidynamic学习/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  uidynamic学习
+//
+//  Created by hejingjin on 16/6/21.
+//  Copyright © 2016年 Chinahr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ uidynamic学习/uidynamic学习Tests/uidynamic__Tests.swift
@@ -0,0 +1,36 @@
+//
+//  uidynamic__Tests.swift
+//  uidynamic学习Tests
+//
+//  Created by hejingjin on 16/6/21.
+//  Copyright © 2016年 Chinahr. All rights reserved.
+//
+
+import XCTest
+@testable import uidynamic__
+
+class uidynamic__Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
